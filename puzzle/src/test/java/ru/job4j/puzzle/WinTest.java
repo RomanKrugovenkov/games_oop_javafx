9453a7d4a1679c49f196c58655cb593a7502cf12
krugo <krugovenkov@mail.ru>
2022-06-22 19:00:40 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
+++ puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
@@ -4,9 +4,8 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-@Ignore
 public class WinTest {
     @Test
     public void whenVerticalWin() {
